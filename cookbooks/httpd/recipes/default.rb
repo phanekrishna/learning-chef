4fd81e083518fb297c91b23911490520d4f585a4
phanekrishna <Phanekrishna.X.Kambhampati.-ND@disney.com>
2015-02-14 20:52:44 -0500

    adding httpd recipe

---
+++ cookbooks/httpd/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'httpd'
+
+service httpd do 
+	action [:start, :enable]
+end
+
